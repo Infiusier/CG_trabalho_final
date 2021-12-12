21002d7399d491e3c3529fa69e3a2a0ee90b2a6f
Tiago Diogenes de Araujo <tiagodiogenes.nepen@gmail.com>
2021-12-12 13:34:07 -0300

    comecando questao 3

--- draw.py
+++ draw.py
@@ -6,8 +6,22 @@ from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 from solids import *
 from typing import List
 
+def criaEixos(pontoMedio, olho):
+    n = np.subtract(pontoMedio, olho)
+    aux = (0, 10, 10)
+    u = np.cross(n, aux)
+    v = np.cross(u, n)
 
-def init_plot(view: str):
+    u = (u / np.linalg.norm(u))[0]
+    v = (v / np.linalg.norm(v))[0]
+    n = (n / np.linalg.norm(n))[0]
+
+    eixos = [u, v, n]
+
+    return eixos
+
+
+def init_plot(view: str = "3d"):
     fig = plt.figure(figsize=(10, 10))
     
     if view == "3d":
@@ -52,10 +66,17 @@ def init_eixos(ax):
     
 
 
-def plot_objetos(objetos: List[Tuple],view: str = "3d"):
+def plot_objetos(objetos: List[Tuple],view: str = "3d", eye: Tuple = None):
     fig, ax = init_plot(view)
     
-    print(view)
+    if eye != None:
+        eye_coordinates = eye[0]
+        eye_axis = eye[1]
+        
+        ax.scatter(eye_coordinates[0], eye_coordinates[1], eye_coordinates[2], c="black")
+        ax.text(eye_coordinates[0], eye_coordinates[1], eye_coordinates[2],"Eye", c="Black")
+        
+        ax.quiver(eye_coordinates[0], eye_coordinates[1], eye_coordinates[2], eye_axis[0], eye_axis[1], eye_axis[2], length=5, normalize=True)
 
     for objeto,color in objetos:
         for linha in objeto.arestas:
@@ -99,38 +120,39 @@ def plot_objetos_q4(objetos: List[Objetos]):
     plt.show()
 
 
-def plot_objetos_q3(objetos: List[Tuple], comp_u, comp_v, comp_n, olho):
-    fig, ax = init_plot()
-
-    ax.scatter(olho[0], olho[1], olho[2], c="black")
-
-    ax.text(olho[0], olho[1], olho[2],"Eye", c="Black")
-
-    for objeto,color in objetos:
-        for linha in objeto.arestas:
-            print(linha)
-            ax.plot(
-                [linha[0][0], linha[1][0]],
-                [linha[0][1], linha[1][1]],
-                zs=[linha[0][2], linha[1][2]],
-            )
-
-        ax.add_collection3d(Poly3DCollection(
-            objeto.faces, facecolors=color, linewidths=1.5, edgecolors=color, alpha=0.1))
-
-    ax.set_zlim3d(-6, 6)
-    ax.set_xlim3d(-6, 6)
-    ax.set_ylim3d(-6, 6)
-
-    ax.set_xlabel("Eixo X")
-    ax.set_ylabel("Eixo Y")
-    ax.set_zlabel("Eixo Z")
-
-    ax.plot([6, -6], [olho[1], olho[1]], [olho[2], olho[2]], color='Black', alpha=0.4)
-    ax.plot([olho[0], olho[0]], [6, -6], [olho[2], olho[2]], color='Black', alpha=0.4)
-    ax.plot([olho[0], olho[0]], [olho[1], olho[1]], [6, -6], color='Black', alpha=0.4)
-
-    plt.show()
+#===============================================================================
+# def plot_objetos_q3(objetos: List[Tuple], comp_u, comp_v, comp_n, olho):
+#     fig, ax = init_plot()
+# 
+#     ax.scatter(olho[0], olho[1], olho[2], c="black")
+#     ax.text(olho[0], olho[1], olho[2],"Eye", c="Black")
+#     
+#     ax.plot([6, -6], [olho[1], olho[1]], [olho[2], olho[2]], color='Black', alpha=0.4)
+#     ax.plot([olho[0], olho[0]], [6, -6], [olho[2], olho[2]], color='Black', alpha=0.4)
+#     ax.plot([olho[0], olho[0]], [olho[1], olho[1]], [6, -6], color='Black', alpha=0.4)
+# 
+#     for objeto,color in objetos:
+#         for linha in objeto.arestas:
+#             print(linha)
+#             ax.plot(
+#                 [linha[0][0], linha[1][0]],
+#                 [linha[0][1], linha[1][1]],
+#                 zs=[linha[0][2], linha[1][2]],
+#             )
+# 
+#         ax.add_collection3d(Poly3DCollection(
+#             objeto.faces, facecolors=color, linewidths=1.5, edgecolors=color, alpha=0.1))
+# 
+#     ax.set_zlim3d(-6, 6)
+#     ax.set_xlim3d(-6, 6)
+#     ax.set_ylim3d(-6, 6)
+# 
+#     ax.set_xlabel("Eixo X")
+#     ax.set_ylabel("Eixo Y")
+#     ax.set_zlabel("Eixo Z")
+# 
+#     plt.show()
+#===============================================================================
 
 
 
--- main.py
+++ main.py
@@ -46,28 +46,46 @@ def main_q2():
     
 def main_q3():
     
-    solids_list = []
-    
     cubo = Paralelepipedo()
     cubo.criaParalelepipedo(pontoInicial =(0.5,0,0),edge = 1.5)
-    solids_list.append((cubo,"red"))
 
     paralelepipedo = Paralelepipedo()
     paralelepipedo.criaParalelepipedo(pontoInicial =(-2,0,0), x=1.5, y=5, z=2.5)
-    solids_list.append((paralelepipedo,"blue"))
-  
+      
     piramide = Piramide()
     piramide.criaPiramide(origin_point = (2.5,0,0), edge = 2, z = 3)
-    solids_list.append((piramide,"black"))
-  
+    
     tronco = PiramideTronco()
     tronco.criaPiramideTronco(origin_point = (-5.5,0,0),lower_edge = 3, upper_edge = 1.3, z = 2.5)
+
+    '''A aaprtir daqui e necessario revisar oq ta acontecendo'''
+    olho = (-5, -5, -5)
+    pontoMedio = Objetos.centroVolumeVisão(cubo, piramide)
+
+    eixos = criaEixos(pontoMedio, olho)
+    inv_eixos = np.linalg.inv(eixos)
+
+    cubo.vertices = np.matmul(inv_eixos, cubo.vertices.T).T
+
+    piramide.vertices = np.matmul(inv_eixos, piramide.vertices.T).T
+
+    paralelepipedo.vertices = np.matmul(inv_eixos, paralelepipedo.vertices.T).T
+
+    tronco.vertices = np.matmul(inv_eixos, tronco.vertices.T).T
+    
+    
+    solids_list = []
+    solids_list.append((cubo,"red"))
+    solids_list.append((paralelepipedo,"blue"))
+    solids_list.append((piramide,"black"))
     solids_list.append((tronco,"green"))
     
-    plot_objetos(solids_list)
-
+    for solid,color in solids_list:
+        solid.update_solid_features()
+    
+    plot_objetos(solids_list, eye = (olho,inv_eixos))
 
 if __name__ == "__main__":
     #main_q1()
-    main_q2()
-    #main_q3()
+    #main_q2()
+    main_q3()
